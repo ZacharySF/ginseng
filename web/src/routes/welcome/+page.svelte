221585f68440c58b7a5212146a904420ad7f98c1
dreams <MichaelnKryvonis@gmail.com>
2026-09-12 15:36:59 -0500

    Slightly enlarge landing frost grain

--- web/src/routes/welcome/+page.svelte
+++ web/src/routes/welcome/+page.svelte
@@ -422,7 +422,7 @@
 		z-index: 2;
 		inset: 0;
 		background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 160 160' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='1.1' numOctaves='2' seed='7' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='160' height='160' filter='url(%23n)'/%3E%3C/svg%3E");
-		background-size: 160px 160px;
+		background-size: 176px 176px;
 		opacity: 0.68;
 		mix-blend-mode: overlay;
 		pointer-events: none;
